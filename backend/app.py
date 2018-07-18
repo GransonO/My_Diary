04d8fc3cd5b4d24f0d99d6f97ac2585b026de61e
GransonO <oyombegranson@gmail.com>
2018-07-18 21:18:25 +0300

    ft-testing api end points with postman-159143040

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-"""Route to home page"""
-@APP.route('/')
-def home():
-    return "This is Andela..."
+"""Call to get all entries"""
+@app.route('/api/v1', methods=['GET'])
+def get_data():
+    return "This is GET..."
+
+"""Call to get a specific entry"""
+@app.route('/api/v1/<string:name>', methods=['GET'])
+def get_specific_data(name):
+    return "This is SPECIFIC GET..." + name
+
+"""call to add an entry"""
+@app.route('/api/v1', methods=['POST'])
+def post_data():
+    return "This is POST..."
+
+"""call to edit an entry"""
+@app.route('/api/v1/<string:user>', methods=['PUT'])
+def put_data(user):
+    return "This is PUT..." + user
 
 if __name__ == '__main__':
-    APP.run(debug=True)
+    app.run(debug=True)
