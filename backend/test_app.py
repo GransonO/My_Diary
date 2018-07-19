45bcbe770dffe9830f2cf0e0366fe9da951d3af1
GransonO <oyombegranson@gmail.com>
2018-07-19 19:48:07 +0300

    ch-Edited the test_allentries function name

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,7 +1,7 @@
 """Pylint testing module"""
 import requests
 
-def test_response():
+def test_allentries():
     """test to get all entries"""
     response = requests.get("http://127.0.0.1:5000/api/v1/entries")
     assert response.status_code, 200
