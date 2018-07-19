8422d0805abf0ee0232d540e105d4b078b5600a3
GransonO <oyombegranson@gmail.com>
2018-07-19 20:27:42 +0300

    ft-fetch a single entry-159176331

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,7 +1,7 @@
 """Pylint testing module"""
 import requests
 
-def test_allentries():
-    """test to get all entries"""
-    response = requests.get("http://127.0.0.1:5000/api/v1/entries")
+def test_singleentries():
+    """test to get a single entry"""
+    response = requests.get("http://127.0.0.1:5000/api/v1/entries/1")
     assert response.status_code, 200
