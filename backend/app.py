d0d86aa6290b4fd1eb4f62abff07951208296908
GransonO <oyombegranson@gmail.com>
2018-07-19 20:04:12 +0300

    ft-Fetch a single entry-159159157

--- backend/app.py
+++ backend/app.py
@@ -11,9 +11,11 @@ def home_page():
     """Redirect to home screen"""
     return '<body style="background-color:white; text-align:center;"><h1 style="margin-top:20px; color:pink;">Geetings, Just some lorem ipsum stuff for you...</h1><div style="text-align:center; margin-left: auto; margin-right: auto;  width:500px; padding:10px"><p style="font-size:20px; color:pink;">Ribeye prosciutto corned beef jowl pig tri-tip, buffalo landjaeger short loin hamburger burgdoggen shankle brisket. Buffalo fatback kevin swine. Chicken jerky swine, pastrami ball tip pork boudin jowl pork belly. Pork flank pork loin buffalo porchetta venison beef ribs meatloaf, tail corned beef. Hamburger pork belly ball tip, landjaeger cow swine short ribs flank ham hock shank sirloin ribeye ham pastrami kevin.Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!</p><img src="https://img.clipartxtras.com/3708e9dd29786aead4df180fd4977d57_happy-piggy-animal-piglets-and-babies-funny-pig-drawing_600-600.png" alt="Happy me" width=250px height=250px></div></body>'
 
-@APP.route('/api/v1/entries', methods=['GET'])
-def get_data():
-    """Call to get all entries"""
-    return jsonify(ENTRIES)
+"""Call to get a specific entry"""
+@APP.route('/api/v1/entries/<int:count>', methods=['GET'])
+def get_specific_data(count):
+    #Get entry the corresponds to sent id
+    result = [entry for entry in ENTRIES if entry['id']==count]
+    return jsonify(result[0])
 if __name__ == '__main__':
     APP.run(debug=True)
