fa23d1bd5900c796f6f5b4c958a4ea887b9453e8
GransonO <oyombegranson@gmail.com>
2018-07-19 19:13:25 +0300

    Editted the url(Added .../entries)

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,5 +3,5 @@ import requests
 
 def test_response():
     """test to get all entries"""
-    response = requests.get("http://127.0.0.1:5000/api/v1")
+    response = requests.get("http://127.0.0.1:5000/api/v1/entries")
     assert response.status_code, 200
