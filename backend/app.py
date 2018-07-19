1cf42ca804839ed2eafc36ea5fbdd1b32c336c38
GransonO <oyombegranson@gmail.com>
2018-07-19 06:26:22 +0300

    ft-Created homepage for web app-159159793

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,12 @@
+"""Flask web server"""
 from flask import Flask
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-"""Call to get all entries"""
-@app.route('/api/v1', methods=['GET'])
-def get_data():
-    return "This is GET..."
-
-"""Call to get a specific entry"""
-@app.route('/api/v1/<string:name>', methods=['GET'])
-def get_specific_data(name):
-    return "This is SPECIFIC GET..." + name
-
-"""call to add an entry"""
-@app.route('/api/v1', methods=['POST'])
-def post_data():
-    return "This is POST..."
-
-"""call to edit an entry"""
-@app.route('/api/v1/<string:user>', methods=['PUT'])
-def put_data(user):
-    return "This is PUT..." + user
+@APP.route('/')
+def home_page():
+    """Redirect to home screen"""
+    return '<body style="background-color:white; text-align:center;"><h1 style="margin-top:20px; color:pink;">Geetings, Just some lorem ipsum stuff for you...</h1><div style="text-align:center; margin-left: auto; margin-right: auto;  width:500px; padding:10px"><p style="font-size:20px; color:pink;">Ribeye prosciutto corned beef jowl pig tri-tip, buffalo landjaeger short loin hamburger burgdoggen shankle brisket. Buffalo fatback kevin swine. Chicken jerky swine, pastrami ball tip pork boudin jowl pork belly. Pork flank pork loin buffalo porchetta venison beef ribs meatloaf, tail corned beef. Hamburger pork belly ball tip, landjaeger cow swine short ribs flank ham hock shank sirloin ribeye ham pastrami kevin.Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!</p><img src="https://img.clipartxtras.com/3708e9dd29786aead4df180fd4977d57_happy-piggy-animal-piglets-and-babies-funny-pig-drawing_600-600.png" alt="Happy me" width=250px height=250px></div></body>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
