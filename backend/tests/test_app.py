76fffb951c2f9c291577a3877bad80e6fbebfdec
GransonO <oyombegranson@gmail.com>
2018-07-21 14:02:45 +0300

    Working on the pytest

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -7,14 +7,13 @@ def test_put_update_data():
     assert response.status_code, 200
 def test_post_data():
     """test add entries"""
-    response = requests.get("http://127.0.0.1:5000/api/v1/entries")
+    response = self.app.get("http://127.0.0.1:5000/api/v1/entries")
     assert response.status_code, 200
 def test_get_all_entries():
     """test to get all entries"""
-    response = requests.get("http://127.0.0.1:5000/api/v1/entries")
+    response = self.app.get("http://127.0.0.1:5000/api/v1/entries")
     assert response.status_code, 200
 def test_get_specific_data():
     """test to get specific entries"""
-    response = requests.get("http://127.0.0.1:5000/api/v1/entries/1")
+    response = self.app.get("http://127.0.0.1:5000/api/v1/entries/1")
     assert response.status_code, 200
-    
