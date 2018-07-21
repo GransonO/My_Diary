0b2c925e823ab65cd0fc3ac2a4e749088ffcc88e
GransonO <oyombegranson@gmail.com>
2018-07-21 16:12:28 +0300

    [chore#159216164]Added more testing points

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,7 +3,8 @@
 def test_put_update_data(client):
     """test to update an entry"""
     response = client.get("http://127.0.0.1:5000/api/v1/entries/1")
-    assert response.status_code, 200
+    assert response.status_code == 200
+    assert response[0].id == 1
 def test_post_data(client):
     """test add entries"""
     response = client.get("http://127.0.0.1:5000/api/v1/entries")
@@ -12,8 +13,8 @@ def test_get_all_entries(client):
     """test to get all entries"""
     response = client.get("http://127.0.0.1:5000/api/v1/entries")
     assert response.status_code, 200
+    assert len(response) == 3
 def test_get_specific_data(client):
     """test to get specific entries"""
     response = client.get("http://127.0.0.1:5000/api/v1/entries/1")
     assert response.status_code, 200
-    
